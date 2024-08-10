ffa14fb348e94fa5aed6063b9000b9cdf7e9cc41
Max Carvajal <jcarvajal288@gmail.com>
2024-08-10 15:28:11 -0500

    Hook stats into the state system

--- modules/gamestate.scm
+++ modules/gamestate.scm
@@ -2,6 +2,7 @@
   #:pure
   #:use-module (scheme base)
   #:use-module (puyo)
+  #:use-module (stdlib debug)
   #:export (initialize-game-state
             revert-board-state!
             current-game-mode
@@ -12,12 +13,17 @@
             set-active-pair-location!
             add-new-board-state!
             switch-mode-to-moving!
-            switch-mode-to-evaluating!))
+            switch-mode-to-evaluating!
+            get-last-chain
+            get-max-chain
+            set-last-chain!
+            set-max-chain!))
 
 (define current-game-mode 'moving)
 (define current-state 0)
 (define grid-timeline #f)
 (define pair-timeline #f)
+(define stats-timeline #f)
 (define active-pair-position1 0)
 (define active-pair-position2 0)
 
@@ -27,7 +33,8 @@
   (set! active-pair-position1 1)
   (set! active-pair-position2 2)
   (set! grid-timeline (make-vector 1 (make-vector grid-length 'empty)))
-  (set! pair-timeline (make-vector 1 (cons (random-puyo-color) (random-puyo-color)))))
+  (set! pair-timeline (make-vector 1 (cons (random-puyo-color) (random-puyo-color))))
+  (set! stats-timeline (make-vector 1 (make-stat-moment 0 0))))
 
 (define (revert-board-state!)
   (if (> current-state 0)
@@ -45,12 +52,6 @@
   (set! active-pair-position1 (car new-location))
   (set! active-pair-position2 (cdr new-location)))
 
-(define (new-active-pair!)
-  (let ((new-pair (cons (random-puyo-color) (random-puyo-color))))
-    (set! pair-timeline (vector-append pair-timeline (vector new-pair)))
-    (set! active-pair-position1 1)
-    (set! active-pair-position2 2)))
-
 (define (update-board!)
   (let ((new-board (vector-copy (get-game-grid)))
         (previous-boards (vector-copy grid-timeline 0 (+ current-state 1)))
@@ -60,13 +61,51 @@
     (vector-set! new-board active-pair-position2 color2)
     (set! grid-timeline (vector-append previous-boards (vector new-board)))))
 
+(define (new-active-pair!)
+  (let ((new-pair (cons (random-puyo-color) (random-puyo-color))))
+    (set! pair-timeline (vector-append pair-timeline (vector new-pair)))
+    (set! active-pair-position1 1)
+    (set! active-pair-position2 2)))
+
 (define (add-new-board-state!)
   (update-board!)
   (new-active-pair!)
+  (new-stat-moment!)
   (set! current-state (+ current-state 1)))
 
-(define(switch-mode-to-evaluating!)
+(define (switch-mode-to-evaluating!)
   (set! current-game-mode 'evaluating))
 
 (define (switch-mode-to-moving!)
   (set! current-game-mode 'moving))
+
+
+;;;; STATS
+
+(define-record-type <stat-moment>
+  (make-stat-moment last-chain max-chain)
+  stat-moment?
+  (last-chain stat-moment-last-chain set-stat-moment-last-chain!)
+  (max-chain stat-moment-max-chain set-stat-moment-max-chain!))
+
+(define (new-stat-moment!)
+  (let* ((current-stat-moment (vector-ref stats-timeline current-state))
+         (new-stat-moment (make-stat-moment
+                            (stat-moment-last-chain current-stat-moment)
+                            (stat-moment-max-chain current-stat-moment)))
+         (previous-stat-moments (vector-copy stats-timeline 0 (+ current-state 1))))
+    (set! stats-timeline (vector-append previous-stat-moments (vector new-stat-moment)))))
+
+(define (get-last-chain)
+  (let ((current-stat-moment (vector-ref stats-timeline current-state)))
+    (stat-moment-last-chain current-stat-moment)))
+
+(define (set-last-chain! new-last-chain)
+  (set-stat-moment-last-chain! (vector-ref stats-timeline current-state) new-last-chain))
+
+(define (get-max-chain)
+  (let ((current-stat-moment (vector-ref stats-timeline current-state)))
+    (stat-moment-max-chain current-stat-moment)))
+
+(define (set-max-chain! new-max-chain)
+  (set-stat-moment-max-chain! (vector-ref stats-timeline current-state) new-max-chain))
--- modules/stats.scm
+++ modules/stats.scm
@@ -3,6 +3,7 @@
   #:use-module (scheme base)
   #:use-module (dom canvas)
   #:use-module (stdlib debug)
+  #:use-module (gamestate)
   #:export (draw-stats
             increment-chain-counter!
             update-chain-stats!))
@@ -11,8 +12,6 @@
 (define stats-y 100.0)
 
 (define chain-counter 0)
-(define last-chain 0)
-(define max-chain 0)
 
 (define (increment-chain-counter!)
   (set! chain-counter (+ chain-counter 1)))
@@ -22,9 +21,9 @@
 
 (define (update-chain-stats!)
   (if (> chain-counter 0)
-      (set! last-chain chain-counter))
-  (if (> chain-counter max-chain)
-      (set! max-chain chain-counter))
+      (set-last-chain! chain-counter))
+  (if (> chain-counter (get-max-chain))
+      (set-max-chain! chain-counter))
   (reset-chain-counter!))
 
 (define (draw-stats context)
@@ -32,6 +31,6 @@
   (set-font! context "24px Eraser")
   (set-text-align! context "left")
   (fill-text context "LAST CHAIN:" stats-x stats-y)
-  (fill-text context (number->string last-chain) (+ stats-x 175) stats-y)
+  (fill-text context (number->string (get-last-chain)) (+ stats-x 175) stats-y)
   (fill-text context "MAX CHAIN:" stats-x (+ stats-y 30))
-  (fill-text context (number->string max-chain) (+ stats-x 175) (+ stats-y 30)))
+  (fill-text context (number->string (get-max-chain)) (+ stats-x 175) (+ stats-y 30)))
